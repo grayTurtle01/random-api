43650872c5bafb404627361be6973219972ca48c
grayTurtle01 <the.correo@protonmail.com>
2022-05-27 06:24:51 -0500

    api/coin

---
+++ index.js
@@ -0,0 +1,22 @@
+express = require('express')
+
+app = express()
+
+app.get("/", (req, res) => {
+    res.end("Hello Express");
+})
+
+app.get("/api/coin", (req,res) => {
+    random = Math.random()
+    flag = random >= 0.5  ? 1 : 0
+
+    obj = { 'random': random,
+            'flag': flag }
+            
+    res.json( obj )
+    
+})
+
+
+PORT = process.env.PORT || 8000
+app.listen(PORT, ()=>console.log(`Server Listen on port: ${PORT}`))
