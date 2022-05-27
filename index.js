0499737ae89207b2d666d276a27fca74eca5ed82
grayTurtle01 <the.correo@protonmail.com>
2022-05-27 06:46:40 -0500

    api/dice

--- index.js
+++ index.js
@@ -20,6 +20,13 @@ app.get("/api/coin", (req,res) => {
     
 })
 
+app.get("/api/dice", (req,res) => {
+    face = Math.ceil( Math.random() *6 )
+
+    obj = { 'face': face }
+    res.json(obj)
+})
+
 
 PORT = process.env.PORT || 8000
 app.listen(PORT, ()=>console.log(`Server Listen on port: ${PORT}`))
